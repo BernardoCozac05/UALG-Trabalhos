10bc76a27979be6cc3f44d24ae0b46cf60066c33
BernardoCozac05 <bernardocozac0@gmail.com>
2025-02-12 16:39:08 +0000

    Push Primeiros Exercicios

---
+++ Exercicios/PrimeiroExe/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+    // declaração pública de um serviço muito simples
+    int add(int a, int b);
+    int multiply(int a, int b);
+    int powerOf(int a, int b);
+#endif // UTILS_H
